a48dbfdd0de5b23bb400714a9c01eb3a653e73aa
Rui Alexandre Agostinho Pinheiro <rui@amiga.local>
2022-12-05 15:52:39 +0100

    initial commit

--- beast-feeder.py
+++ beast-feeder.py
@@ -13,7 +13,7 @@ import functools
 # TITLE ---------------------------
 BUILD_MAJOR = '13'
 BUILD_DATE = '221205' # this is the fall-back date for versioning
-BUILD_MINOR = '01'
+BUILD_MINOR = '02'
 TITLE = 'SKYSQUITTER BEAST-FEEDER'
 VERSION_FILENAME = '/.VERSION.beast-feeder'
 # ---------------------------------
