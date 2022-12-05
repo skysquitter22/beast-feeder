7dade206c435a550efdfcf47aa2c5931505d1e1d
Rui Alexandre Agostinho Pinheiro <rui@amiga.local>
2022-12-05 16:03:47 +0100

    test

--- beast-feeder.py
+++ beast-feeder.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 # pylint: disable=C0103,C0114,C0112,C0116,W1514,W0702
 
-""" beast-feeder.py <recv_host> <recv_port> <dest_host> <dest_port> <gps_avail> """
+""" beast-feeder.py <recv_host> <recv_port> <dest_host> <dest_port> """
 
 # LIBS ---------
 import signal
 import socket
 import sys
 import functools
-import datetime
 # --------------
 
 # TITLE ---------------------------
-BUILD_MAJOR = '13'
-BUILD_DATE = '221205' # this is the fall-back date for versioning
-BUILD_MINOR = '01'
+BUILD_MAJOR = '11'
+BUILD_DATE = '220806' # this is the fall-back date for versioning
+BUILD_MINOR = '02'
 TITLE = 'SKYSQUITTER BEAST-FEEDER'
 VERSION_FILENAME = '/.VERSION.beast-feeder'
 # ---------------------------------
@@ -24,7 +23,6 @@ RECV_HOST = 'readsb'
 RECV_PORT = 30005
 DEST_HOST = '10.9.2.1'
 DEST_PORT = 11092
-GPS_AVAIL = False
 # ----------------------------
 
 # CONSTANTS ------------------
@@ -34,21 +32,18 @@ MSG_TYPE_1 = 0x31
 MSG_TYPE_2 = 0x32
 MSG_TYPE_3 = 0x33
 MSG_TYPE_4 = 0x34
-TIMESTAMP_LEN = 6
-TIMESTAMP_INDEX = 2
 # Buffer
 BUFFER_SIZE = 64
 # ----------------------------
 
 # VARIABLES ---------------------
-buffer = []
+buffer = bytearray(BUFFER_SIZE)
 buffer_index = 0
 # Set defaults
 recv_host = RECV_HOST
 recv_port = RECV_PORT
 dest_host = DEST_HOST
 dest_port = DEST_PORT
-gps_avail = GPS_AVAIL
 # -------------------------------
 
 # ensure print always flushes the buffer:
@@ -84,14 +79,15 @@ def sigterm_handler():
     shutdown_gracefully()
 
 def preamble_detected():
-    """ Return True if message preamble detected """
+    """ Return 1 if message preamble detected """
+    ## global buffer_index
     index = buffer_index - 1
     # Check message type
     try:
         if buffer[index] != MSG_TYPE_1 and buffer[index] != MSG_TYPE_2 and \
                         buffer[index] != MSG_TYPE_3 and \
                         buffer[index] != MSG_TYPE_4:
-            return False
+            return 0
     except IndexError:
         # This error is almost always caused by losing the connection to the RECV_HOST.
         print("Beast-feeder is exiting - did we lose the connection to " \
@@ -104,21 +100,21 @@ def preamble_detected():
         esc_count += 1
         index -= 1
     if index == 0 and buffer[index] == ESCAPE_BYTE:
-        return False
+        return 0
     if esc_count % 2 == 0:
-        return False
-    return True
+        return 0
+    return 1
 
 def msg_is_valid(message):
-    """ Return True if message is to be sent; check message preamble and type;
+    """ Return 1 if message is to be sent; check message preamble and type;
        ESC byte at beginning required """
     if message[0] != ESCAPE_BYTE:
-        return False
+        return 0
     # Either message tyoe 2 or 3 required
     if message[1] != MSG_TYPE_2 and message[1] != MSG_TYPE_3:
-        return False
+        return 0
     # Message preamble and type is valid -> send to destination
-    return True
+    return 1
 
 def connect_to_receiver():
     """ Connect to the Receiver server via TCP"""
@@ -167,102 +163,33 @@ def send_to_destination(message):
 def process_recv_bytes(recv_bytes):
     """ Process received byte """
     global buffer_index         # pylint: disable=W0603
+    # Avoid buffer overflow
+    if buffer_index == BUFFER_SIZE:
+        buffer_index = 0
     # Add received data chunk to buffer
-    buffer.extend(recv_bytes)
-    buffer_index += len(recv_bytes)
+    buffer[buffer_index:buffer_index + 1] = recv_bytes
+    buffer_index += 1
+    if buffer_index < 3:
+        return
     # Look for Beast preamble
     if preamble_detected():
         # Prepare received message
-        message = bytearray(buffer[0:buffer_index - 2])
+        message = bytearray(buffer_index - 2)
+        message = buffer[0:buffer_index - 2]
         # Send message
         if msg_is_valid(message):
-            if gps_avail == False:
-                message = get_new_timestamped_message(message)
             send_to_destination(message)
-        # Reset buffer and set preamble in new message buffer
-        preamble = [buffer[buffer_index - 2], buffer[buffer_index - 1]]
-        buffer.clear()
-        buffer.extend(preamble)
-        buffer_index = len(preamble)   
-
-def get_new_timestamped_message(message):
-    """ Insert the system time as timestamp and return the mew message """
-    timestamp_buffer = get_timestamp_buffer()
-     # Find timestamp begin and end index
-    index = TIMESTAMP_INDEX
-    counter = 0
-    while counter < TIMESTAMP_LEN:
-        if message[index] == ESCAPE_BYTE:
-            index += 1
-        index +=1
-        counter += 1
-    signalIndex = index
-    # Create new message
-    new_message_len = 2 + len(timestamp_buffer) + len(message[signalIndex:])
-    new_message = bytearray(new_message_len)
-    # Preamble
-    new_message.extend(message[0:2])
-    # New timestamp
-    new_message.extend(timestamp_buffer)
-    # Remaining orginal message
-    new_message.extend(message[signalIndex:])
-    return new_message
-        
-def get_timestamp_buffer():
-    """ Build and return an actual timestamp buffer """
-   # Get actual time values
-    now = datetime.datetime.now()
-    midnight = datetime.datetime.combine(now.date(), datetime.time())
-    secs_of_day = (now - midnight).seconds
-    nanos_of_sec = now.microsecond * 1000
-    # Build timestamp
-    buffer = []
-    byte_counter = 0
-    # Secs
-    buffer.append(secs_of_day >> 10)
-    byte_counter += 1
-    if buffer[len(buffer) - 1] == ESCAPE_BYTE:
-        buffer.append(ESCAPE_BYTE)
-        byte_counter += 1
-    secs_of_day = secs_of_day - (buffer[len(buffer) - 1] << 10)
-    buffer.append(secs_of_day >> 2)
-    byte_counter += 1
-    if buffer[len(buffer) - 1] == ESCAPE_BYTE:
-        buffer.append(ESCAPE_BYTE)
-        byte_counter += 1
-    secs_of_day = secs_of_day - (buffer[len(buffer) - 1]  << 2)
-    byte2= secs_of_day << 6
-    # Nanos
-    buffer.append(byte2 + (nanos_of_sec >> 24))
-    byte_counter += 1
-    if buffer[len(buffer) - 1] == ESCAPE_BYTE:
-        buffer.append(ESCAPE_BYTE)
-        byte_counter += 1
-    nanos_of_sec = nanos_of_sec - ((buffer[len(buffer) - 1] & 0x3f) << 24)
-    buffer.append(nanos_of_sec >> 16)
-    byte_counter += 1
-    if buffer[len(buffer) - 1] == ESCAPE_BYTE:
-        buffer.append(ESCAPE_BYTE)
-        byte_counter += 1
-    nanos_of_sec = nanos_of_sec - (buffer[len(buffer) - 1] << 16)
-    buffer.append(nanos_of_sec >> 8)
-    byte_counter += 1
-    if buffer[len(buffer) - 1] == ESCAPE_BYTE:
-        buffer.append(ESCAPE_BYTE)
-        byte_counter += 1
-    nanos_of_sec = nanos_of_sec - (buffer[len(buffer) - 1] << 8)
-    buffer.append(nanos_of_sec)
-    byte_counter += 1
-    if buffer[len(buffer) - 1] == ESCAPE_BYTE:
-        buffer.append(ESCAPE_BYTE)
-        byte_counter += 1
-    return bytearray(buffer[0:byte_counter])
+        # Reset buffer
+        buffer[0] = buffer[buffer_index - 2]
+        buffer[1] = buffer[buffer_index - 1]
+        buffer_index = 2
 
 def listen_to_receiver():
     """ Listen for incoming bytes from the Receiver """
     print('Start listening...')
     while 1:
-        recv_bytes = bytearray(sock_recv.recv(1))
+        recv_bytes = bytearray(1)
+        recv_bytes = sock_recv.recv(1)
         process_recv_bytes(recv_bytes)
 
 def process_args():
@@ -273,33 +200,25 @@ def process_args():
     global recv_port
     global dest_host
     global dest_port
-    global gps_avail
     # Get number of arguments
-    args_len = len(sys.argv)
+    args_len = len(sys.argv) - 1
     # Set RECEIVER host
-    if args_len > 1:
+    if args_len >= 1:
         recv_host = sys.argv[1]
     # Set RECEIVER port
-    if args_len > 2:
+    if args_len >= 2:
         recv_port = int(sys.argv[2])
     # Set DESTINATION host
-    if args_len > 3:
+    if args_len >= 3:
         dest_host = sys.argv[3]
     # Set DESTINATION port
-    if args_len > 4:
+    if args_len >= 4:
         dest_port = int(sys.argv[4])
-    # Set GPS available
-    if args_len > 5:
-        gps_avail = strIsTrue(sys.argv[5])
     print('Recv host: ' + recv_host)
     print('Recv port: ' + str(recv_port))
     print('Dest host: ' + dest_host)
     print('Dest port: ' + str(dest_port))
-    print('GPS avail: ' + str(gps_avail))
     print()
-    
-def strIsTrue(str):
-    return str.lower() in ('true', '1', 'yes', 'y')
 # ------------------------------------------------------------------
 
 # EXECUTE ----------------------------------------------------------
@@ -307,7 +226,6 @@ def strIsTrue(str):
 print()
 print(TITLE)
 print('build ' + BUILD)
-print('TEST 1')
 print()
 
 # Process start arguments
@@ -332,3 +250,4 @@ connect_to_receiver()
 # Start worker, listening to Receiver server
 listen_to_receiver()
 # ------------------------------------------------------------------
+
