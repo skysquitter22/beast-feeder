7bcfb61fbe07893dc0e441dd1d31bb4594cd9626
Rui Alexandre Agostinho Pinheiro <rui@amiga.local>
2022-12-05 15:24:02 +0100

    bugfixing not ref buffer_index

--- beast-feeder.py
+++ beast-feeder.py
@@ -14,7 +14,7 @@ import datetime
 # TITLE ---------------------------
 BUILD_MAJOR = '13'
 BUILD_DATE = '221205' # this is the fall-back date for versioning
-BUILD_MINOR = '08'
+BUILD_MINOR = '09'
 TITLE = 'SKYSQUITTER BEAST-FEEDER'
 VERSION_FILENAME = '/.VERSION.beast-feeder'
 # ---------------------------------
