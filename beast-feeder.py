c4f1ac1aff22af650ae3ae565b89301d3ad85f21
Rui Alexandre Agostinho Pinheiro <rui@Ruis-MacBook-Pro.fritz.box>
2023-09-22 01:28:19 +0200

    debugging df_filter str

--- beast-feeder.py
+++ beast-feeder.py
@@ -127,15 +127,15 @@ def process_args():
         set_timestamp = str_is_true(sys.argv[5])
     if args_len > 6:
         clock_diff_limit = int(sys.argv[6])
-#    if args_len > 7:
-#        df_filter = get_int_array_from_str(sys.argv[7])
+    if args_len > 7:
+        df_filter = get_int_array_from_str(sys.argv[7])
     print('Recv host: ' + recv_host)
     print('Recv port: ' + str(recv_port))
     print('Dest host: ' + dest_host)
     print('Dest port: ' + str(dest_port))
     print('Set Timestamp: ' + str(set_timestamp))
     print('Clock diff limit: ' + str(clock_diff_limit) + 'ms')
-    print('DF filter: ' + get_str_of_df_filter())
+#    print('DF filter: ' + get_str_of_df_filter())
     print()
 
 def shutdown_gracefully():
